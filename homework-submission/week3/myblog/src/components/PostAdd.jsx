29dfc531d9737c33fb30be65f7ef09c9ecb4c519
NN <nelson.najarro@gmail.com>
2019-10-30 17:50:03 +0100

    Add functions and useEffect to post form (Have some errors)

--- homework-submission/week3/myblog/src/components/PostAdd.jsx
+++ homework-submission/week3/myblog/src/components/PostAdd.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Form,
   Input,
@@ -6,23 +6,63 @@ import {
   Card
 } from 'antd';
 
+const PostAdd = () => {
+  const [postTitle, setPostTitle] = useState('');
+  const [postContent, setPostContent] = useState('');
+  const [postForm, setPostForm] = useState(false);
 
-const PostAdd = () => (
-  <div style={{ background: '#ECECEC', padding: '30px' }}>
-    <Card title="Add Post" bordered={false} style={{ width: '70%', margin: 'auto' }}>
-      <Form.Item   >
-        <Input
-          type="text"
-          placeholder="Post Title"
-        />
-        <Input.TextArea
-          type="textarea"
-          placeholder="Write your post"
-        />
-        <Button type="primary" htmlType="submit" >    Add Post</Button>
-      </Form.Item >
-    </Card>
-  </div>
-);
+  useEffect((postTitle, postContent) => {
+    let url = 'http://142.93.51.96/posts';
+    if (postForm === true) {
+      fetch(url, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({ title: postTitle, content: postContent })
+      })
+        .then(res => res.json())
+        .then(response => {
+          console.log(response);
+        })
+    }
+  }, [postForm]);
+
+  return (
+    <div style={{ background: '#ECECEC', padding: '30px' }}>
+      <Card title="Add Post" bordered={false} style={{ width: '70%', margin: 'auto' }}>
+        <Form onSubmit={(event) => {
+          event.preventDefault();
+          setPostForm(true);
+        }}>
+          <Form.Item  >
+            <Input
+              type="text"
+              placeholder="Post Title"
+              value={postTitle} onChange={(event) => {
+                event.preventDefault();
+                setPostTitle(event.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item  >
+            <Input.TextArea
+              type="textarea"
+              placeholder="Write your post"
+              value={postContent} onChange={(event) => {
+                event.preventDefault();
+                setPostContent(event.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" >    Add Post</Button>
+          </Form.Item >
+        </Form>
+      </Card>
+    </div >
+  )
+};
 
 export default PostAdd;
