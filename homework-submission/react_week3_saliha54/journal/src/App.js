aceba17f4a908fa1635c973d6e1cc24e70186bc7
Saliha <salihayaylaci@g.ucla.edu>
2019-11-02 22:53:33 +0100

    th

--- homework-submission/react_week3_saliha54/journal/src/App.js
+++ homework-submission/react_week3_saliha54/journal/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import 'antd/dist/antd.css';
 import './App.css';
+
+import Home from './Home.jsx';
+import Details from './Details.jsx';
+import Create from './Create.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="elements">
+          <Switch>
+            <Route exact path="/">
+              <Home data={data} />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+            <Route path="/:id">
+              <Details data={data} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
